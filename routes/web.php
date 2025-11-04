ca74f7251452c2c3bee6db07b4af9ca6e52714c4
mannuccimattia <mannuccimattia@gmail.com>
2025-11-04 13:25:32 +0100

    [FEAT] Added empty routes and pages

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::view('/', 'home')->name('home');
+Route::view('/about', 'about')->name('about');
+Route::view('/contacts', 'contacts')->name('contacts');
